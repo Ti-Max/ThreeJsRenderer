5af35d489e908fb04a37cb396a3dde98b26bd8df
Ti-Max <maxtimofy@gmail.com>
2021-11-23 22:37:02 +0100

    Testing light. (actually testing branches, all smal changes are in dev branch)

--- src/app.js
+++ src/app.js
@@ -10,15 +10,24 @@ document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
 const cube = new THREE.Mesh( geometry, material );
+cube.position.y = -2;
 scene.add( cube );
 
 camera.position.z = 5;
 
 
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene.add( directionalLight );
+const targetObject = new THREE.Object3D();
+scene.add(targetObject);
+
+directionalLight.target = targetObject;
+
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 animate();
