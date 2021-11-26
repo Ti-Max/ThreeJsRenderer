8a4b22d90abbcf42a14fdb33b465088f6dd84cd4
Ti-Max <maxtimofy@gmail.com>
2021-11-26 20:40:36 +0100

    trying models export

--- src/app.js
+++ src/app.js
@@ -1,33 +1,63 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {GUI} from 'dat.gui'
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-const cube = new THREE.Mesh( geometry, material );
-cube.position.y = -2;
-scene.add( cube );
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+	// resource URL
+	'models/scene.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+
 
 camera.position.z = 5;
-
-
-
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-scene.add( directionalLight );
-const targetObject = new THREE.Object3D();
-scene.add(targetObject);
-
-directionalLight.target = targetObject;
+//------------------------------------------------
+//GUI
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Model')
+// cubeFolder.add(model.rotation, 'x', 0, Math.PI * 2)
+// cubeFolder.add(model.rotation, 'y', 0, Math.PI * 2)
+// cubeFolder.add(model.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.open()
+//------------------------------------------------
 
 function animate() {
 	requestAnimationFrame( animate );
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+
 	renderer.render( scene, camera );
 }
 animate();
