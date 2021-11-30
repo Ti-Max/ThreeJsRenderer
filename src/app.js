ff4877239e0dd8b96e2a313f3d26261ca1d6da7f
Ti-Max <maxtimofy@gmail.com>
2021-11-30 14:14:55 +0100

    starting models loaders

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import {GUI} from 'lil-gui'
-import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js';
-import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass.js';
-import * as VerticalBlur from './VerticalBlurShader.hlsl.js';
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader.js';
 
 let scene; 
 let camera;
@@ -22,7 +20,7 @@ animate();
 
 function init() {
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 500 );
 	renderer = new THREE.WebGLRenderer({antialias : true});
 	orbitControl = new OrbitControls(camera, renderer.domElement);
 
@@ -33,25 +31,9 @@ function init() {
 	//Renderer settings
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-	scene.fog = new THREE.Fog(0xff9999, 10, 20)
+	scene.fog = new THREE.Fog(0xff9999, 50, 500)
 	scene.background = new THREE.Color(0xff9999);
 
-	//ShaderPass
-	composer = new EffectComposer(renderer);
-	composer.addPass(new RenderPass(scene, camera));
-
-	const colorShader = {
-		uniforms: {
-		  tDiffuse: { value: null },
-		},
-		vertexShader: VerticalBlur.vertexShader,
-		fragmentShader: VerticalBlur.fragmentShader,
-	};
-
-
-	const colorPass = new ShaderPass(colorShader);
-	colorPass.renderToScreen = true;
-	composer.addPass(colorPass);
 	//Lights
 	createLights();
 	//-------------Models
@@ -59,7 +41,7 @@ function init() {
 	//GUI
 	buildGui();
 
-	camera.position.z = -5;
+	camera.position.set(0, 30, -30);
 	
 }
 
@@ -69,7 +51,7 @@ function animate(time) {
 	lastTime = time;
 	orbitControl.update();
 
-	composer.render(deltaTime);
+	renderer.render(scene, camera);
 
 	requestAnimationFrame( animate );
 }
@@ -98,34 +80,6 @@ function createLights(){
 	dirLight1.shadow.mapSize.set(512, 512);
 	scene.add( dirLight1 );
 
-	// const ligthHelper = new THREE.DirectionalLightHelper(dirLight1);
-	// scene.add(ligthHelper);
-
-	// //Pointlight
-	// const pointLight = new THREE.PointLight( 0x5555ff, 1, 20);
-	// pointLight.position.set(2, 4, 2);
-	// pointLight.castShadow = true;
-	// pointLight.shadow.bias = 0.0001;
-	// pointLight.shadow.mapSize.set(512, 512);
-	// scene.add( pointLight );
-
-	// const pointLightHelper = new THREE.CameraHelper(pointLight.shadow.camera);
-	// scene.add(pointLightHelper);
-	
-	// //SpotLight
-	// const spotLight = new THREE.SpotLight( 0x00ff00, 1, 20);
-	// spotLight.position.set(4, 4, 0);
-	// spotLight.target.position.set(0, 0, 2);
-	// spotLight.castShadow = true;
-	// spotLight.shadow.bias = 0.0001;
-	// spotLight.shadow.mapSize.set(512, 512);
-
-	// scene.add( spotLight );
-	// scene.add(spotLight.target);
-
-	// const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0.2);
-	// scene.add(spotLightHelper);
-
 	//GUI
 	const folder1 = gui.addFolder("Directional Light");
 	folder1.add(dirLight1, 'intensity', 0, 5, 0.1);
@@ -135,9 +89,19 @@ function createLights(){
 
 }
 function createModels(){
+	const mtlLoader = new MTLLoader();
+	const objLoader = new OBJLoader();
+	mtlLoader.load('models/car2/car2.mtl', (mtl) => {
+	  mtl.preload();
+	  objLoader.setMaterials(mtl);
+	  objLoader.load('models/car2/car2.obj', (root) => {
+		scene.add(root);
+	  });
+	});
+
 	//plane
 	const plane = new THREE.Mesh(
-		new THREE.PlaneGeometry( 100, 100 ),
+		new THREE.PlaneGeometry( 1000, 1000 ),
 		new THREE.MeshPhongMaterial( { color: 0xAA9999, specular: 0x101010 } )
 	);
 	plane.rotation.x = - Math.PI / 2;
@@ -145,32 +109,32 @@ function createModels(){
 
 	scene.add( plane );
 
-	//cube
-	const cube = new THREE.Mesh(
-		new THREE.BoxGeometry(0.4, 0.4, 0.4),
-		new THREE.MeshPhongMaterial({color : 0x2288ff})
-	);
-	cube.castShadow = true;
-	cube.position.y = 1;
-	scene.add(cube);
+	// //cube
+	// const cube = new THREE.Mesh(
+	// 	new THREE.BoxGeometry(0.4, 0.4, 0.4),
+	// 	new THREE.MeshPhongMaterial({color : 0x2288ff})
+	// );
+	// cube.castShadow = true;
+	// cube.position.y = 1;
+	// scene.add(cube);
 
-	//sphere
-	const sphere = new THREE.Mesh(
-		new THREE.SphereGeometry(1),
-		new THREE.MeshPhongMaterial({color : 0xff4455})
-	)
-	sphere.position.set(2, 1, 0);
-	sphere.castShadow = true;
-	scene.add(sphere);
+	// //sphere
+	// const sphere = new THREE.Mesh(
+	// 	new THREE.SphereGeometry(1),
+	// 	new THREE.MeshPhongMaterial({color : 0xff4455})
+	// )
+	// sphere.position.set(2, 1, 0);
+	// sphere.castShadow = true;
+	// scene.add(sphere);
 	
-	//sphere
-	const torus = new THREE.Mesh(
-		new THREE.TorusKnotGeometry(0.5, 0.2),
-		new THREE.MeshPhongMaterial({color : 0x00CC33})
-	)
-	torus.position.set(1, 1, 2);
-	torus.castShadow = true;
-	scene.add(torus);
+	// //sphere
+	// const torus = new THREE.Mesh(
+	// 	new THREE.TorusKnotGeometry(0.5, 0.2),
+	// 	new THREE.MeshPhongMaterial({color : 0x00CC33})
+	// )
+	// torus.position.set(1, 1, 2);
+	// torus.castShadow = true;
+	// scene.add(torus);
 }
 
 function buildGui(){
@@ -178,8 +142,6 @@ function buildGui(){
 
 	ligthfolder.add(dirLight1, 'castShadow');
 	ligthfolder.open();
-	// gui.add(dirLight1.shadow., 'width', 10, 5000);
-	// gui.add(dirLight1.shadow.camera, 'height', 10, 5000);
 }
 function makeXYZLightGUI(folder, vector3, onChangeFn) {
 
