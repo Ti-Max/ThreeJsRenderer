a14cefdb29e54f31b38f44bd5dc6e986324dc4cf
Ti-Max <maxtimofy@gmail.com>
2021-12-06 13:50:45 +0100

    Finished thing

--- src/app.js
+++ src/app.js
@@ -12,7 +12,15 @@ let dirLight1;
 const gui = new GUI();
 let lastTime = 0;
 
-
+const models = {
+	car1 : 'car3/scene.gltf',
+	car2 : 'car4/scene.gltf',
+	car3 : 'old_rusty_car/scene.gltf'
+}
+const loadedModels = new Map();
+const selectedModel = {
+	model : models.car1
+}
 init();
 animate();
 
@@ -28,20 +36,17 @@ function init() {
 	window.addEventListener( 'resize', onWindowResize );
 
 	//Renderer settings
-	renderer.shadowMap.enabled = true;
 	renderer.outputEncoding = THREE.sRGBEncoding;
 	renderer.toneMapping = THREE.ACESFilmicToneMapping;
 	renderer.toneMappingExposure = 1;
-	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 
 	//Lights
 	createLights();
 	//-------------Models
-	createModels();
 	//GUI
 	buildGui();
-
+	loadModel('car3/scene.gltf');
 	// Creates Environment maps and gui for them
 	Environment.init(scene, renderer, gui);
 
@@ -77,36 +82,66 @@ function createLights(){
 	dirLight1 = new THREE.DirectionalLight( 0xffffff, 1 );
 	dirLight1.position.y = 10;
 	dirLight1.target.position.set(0, 0, 0);
-	dirLight1.castShadow = true;
-	dirLight1.shadow.mapSize.set(512, 512);
 	scene.add( dirLight1 );
 
 	//GUI
 	const folder1 = gui.addFolder("Directional Light");
 	folder1.add(dirLight1, 'intensity', 0, 5, 0.1);
 }
-function createModels(){
+
+function loadModel(path){
 
 	const loader = new GLTFLoader();
-	loader.load("./models/car3/scene.gltf", function ( gltf ) {
-		// gltf.scene.traverse( function( node ) {
+	let model;
+	loader.load("./models/" + path, function ( gltf ) {
 
-		// 	if ( node.isMesh ) { node.castShadow = true; node.receiveShadow = true; }
-	
-		// } );
 		scene.add( gltf.scene );
-	
+		const box = new THREE.Box3().setFromObject( gltf.scene);
+ 
+		const boxSize = box.getSize(new THREE.Vector3()).length();
+		const boxCenter = box.getCenter(new THREE.Vector3());
+	 
+		// set the camera to frame the box
+		frameArea(boxSize * 1.2, boxSize, boxCenter, camera);
+	 
+		// update the Trackball controls to handle the new size
+		orbitControl.maxDistance = boxSize * 10;
+		orbitControl.target.copy(boxCenter);
+		orbitControl.update();
+		model = gltf.scene;
 	}, undefined, function ( error ) {
 	
 		console.error( error );
-	
+		return 0;
 	} )
+	return model;
 }
+function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
+	const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+	const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+	const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+   
+	const direction = (new THREE.Vector3()).subVectors(camera.position, boxCenter).normalize();
+   
+	camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+   
 
+	camera.near = boxSize / 100;
+	camera.far = boxSize * 100;
+   
+	camera.updateProjectionMatrix();
+   
+	camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+  }
+  
 function buildGui(){
-	const ligthfolder = gui.addFolder('light');
-	ligthfolder.add(dirLight1, 'castShadow');
-	ligthfolder.open();
 	
+	const modelsFolder = gui.addFolder('models');
+	modelsFolder.add(selectedModel, 'mdoel',
+	{Car1 : models.car1, Car2: models.car2})
+	.onChange(function(value){
+			loadModel(value);
+	});
+
 	console.log();
 }
